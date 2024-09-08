586286dcb2deb183e2417eb59088f9cfcc01f419
shafakyildiz <shafakyildiz@gmail.com>
2024-09-08 16:28:14 +0300

    feat: add update and delete methods

--- src/Play.Catalog/Play.Catalog.Service/Controllers/ItemsController.cs
+++ src/Play.Catalog/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -39,5 +39,32 @@ namespace Play.Catalog.Service.Controllers
 
             return CreatedAtAction(nameof(GetById), new { id = item.Id }, item);
         }
+
+        [HttpPut]
+        public IActionResult Put(Guid id, UpdateItemDto updateItemDto)
+        {
+            var existingItem = items.Where(item => item.Id == id).SingleOrDefault();
+
+            var updatedItem = existingItem with
+            {
+                Name = updateItemDto.Name,
+                Description = updateItemDto.Description,
+                Price = updateItemDto.Price,
+            };
+
+            var index = items.FindIndex(existingItem => existingItem.Id == id);
+            items[index] = updatedItem;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var index = items.FindIndex(item => item.Id == id);
+            items.RemoveAt(index);
+
+            return NoContent();
+        }
     }
 }

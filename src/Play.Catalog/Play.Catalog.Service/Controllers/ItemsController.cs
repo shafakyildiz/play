2644729aadb9d463b46c3c57884c183fa40922d1
shafakyildiz <shafakyildiz@gmail.com>
2024-09-08 16:19:23 +0300

    feat: add create item method

--- src/Play.Catalog/Play.Catalog.Service/Controllers/ItemsController.cs
+++ src/Play.Catalog/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -9,7 +9,7 @@ namespace Play.Catalog.Service.Controllers
     // https://localhost:5001/items
     [ApiController]
     [Route("items")]
-    public class ItemsController : ControllerBase 
+    public class ItemsController : ControllerBase
     {
         private static readonly List<ItemDto> items = new()
         {
@@ -18,8 +18,8 @@ namespace Play.Catalog.Service.Controllers
             new ItemDto(Guid.NewGuid(), "Bronze Sword", "Deals a small amount of damage", 20, DateTimeOffset.UtcNow),
         };
 
-        [HttpGet]    
-        public IEnumerable<ItemDto> Get() 
+        [HttpGet]
+        public IEnumerable<ItemDto> Get()
         {
             return items;
         }
@@ -29,6 +29,15 @@ namespace Play.Catalog.Service.Controllers
         {
             var item = items.Where(item => item.Id == id).SingleOrDefault();
             return item;
+        }
+
+        [HttpPost]
+        public ActionResult<ItemDto> Post(CreateItemDto createItemDto)
+        {
+            var item = new ItemDto(Guid.NewGuid(), createItemDto.Name, createItemDto.Description, createItemDto.Price, DateTimeOffset.UtcNow);
+            items.Add(item);
+
+            return CreatedAtAction(nameof(GetById), new { id = item.Id }, item);
         }
     }
 }

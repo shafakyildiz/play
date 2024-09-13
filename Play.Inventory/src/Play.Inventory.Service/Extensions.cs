4e477590b1d44fddf2f69df3d05acafefc89d571
shafakyildiz <shafakyildiz@gmail.com>
2024-09-14 00:09:16 +0300

    feat: implement extensions class

---
+++ Play.Inventory/src/Play.Inventory.Service/Extensions.cs
@@ -0,0 +1,13 @@
+using Play.Inventory.Service.Entities;
+using Play.Inventory.Service.Dtos;
+
+namespace Play.Inventory.Service
+{
+    public static class Extensions
+    {
+        public static InventoryItemDto AsDto(this InventoryItem item)
+        {
+            return new InventoryItemDto(item.CatalogItemId, item.Quantity, item.AcquiredDate);
+        }
+    }
+}

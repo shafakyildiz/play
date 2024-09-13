c00ccc0876050b225a584fdd8854b7f291ce8688
shafakyildiz <shafakyildiz@gmail.com>
2024-09-14 00:09:07 +0300

    feat: implement dtos

--- Play.Inventory/src/Play.Inventory.Service/Dtos.cs
+++ Play.Inventory/src/Play.Inventory.Service/Dtos.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace Play.Inventory.Service
+namespace Play.Inventory.Service.Dtos
 {
     public record GraantItemsDto(Guid UserId, Guid CatalogItemId, int Quantity);
     public record InventoryItemDto(Guid CatalogItemId, int Quantity, DateTimeOffset AcquiredDate);

7fed1667c87c745910c4570957ef3ba3ae51a05d
shafakyildiz <shafakyildiz@gmail.com>
2024-09-15 13:07:02 +0300

    feat: add random jitterer to changing request times

--- Play.Inventory/src/Play.Inventory.Service/Startup.cs
+++ Play.Inventory/src/Play.Inventory.Service/Startup.cs
@@ -31,11 +31,14 @@ namespace Play.Inventory.Service
         {
             services.AddMongo().AddMongoRepository<InventoryItem>("inventoryitems");
 
+            Random jitterer = new Random();
+
             services.AddHttpClient<CatalogClient>(client =>
             {
                 client.BaseAddress = new Uri("https://localhost:5001");
             }).AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
-                5, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                5, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
+                + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000)),
                 onRetry: (outcome, timespan, retryAttempt) =>
                 {
                     var serviceProvider = services.BuildServiceProvider();

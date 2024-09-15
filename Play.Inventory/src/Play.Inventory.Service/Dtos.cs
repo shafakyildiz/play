d75ed56c50cb4e3e9d7efd8247725d03e288fcc9
shafakyildiz <shafakyildiz@gmail.com>
2024-09-15 12:21:29 +0300

    feat: add CatalogItemDto to dtos class

--- Play.Inventory/src/Play.Inventory.Service/Dtos.cs
+++ Play.Inventory/src/Play.Inventory.Service/Dtos.cs
@@ -4,4 +4,5 @@ namespace Play.Inventory.Service.Dtos
 {
     public record GraantItemsDto(Guid UserId, Guid CatalogItemId, int Quantity);
     public record InventoryItemDto(Guid CatalogItemId, int Quantity, DateTimeOffset AcquiredDate);
+    public record CatalogItemDto(Guid Id, string Name, string Description);
 }

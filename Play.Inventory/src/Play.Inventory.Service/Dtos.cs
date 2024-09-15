c3ae63c59d9f2ba9eebc01113d90c8921cccadb2
shafakyildiz <shafakyildiz@gmail.com>
2024-09-15 12:31:42 +0300

    fix: fixage on InventoryItemDto

--- Play.Inventory/src/Play.Inventory.Service/Dtos.cs
+++ Play.Inventory/src/Play.Inventory.Service/Dtos.cs
@@ -3,6 +3,6 @@ using System;
 namespace Play.Inventory.Service.Dtos
 {
     public record GraantItemsDto(Guid UserId, Guid CatalogItemId, int Quantity);
-    public record InventoryItemDto(Guid CatalogItemId, int Quantity, DateTimeOffset AcquiredDate);
+    public record InventoryItemDto(Guid CatalogItemId, string Name, string Description, int Quantity, DateTimeOffset AcquiredDate);
     public record CatalogItemDto(Guid Id, string Name, string Description);
 }

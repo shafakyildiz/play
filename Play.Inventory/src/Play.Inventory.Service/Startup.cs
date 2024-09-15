2fd555e7de467d2b148c99e1b3643ca4fdad9bc9
shafakyildiz <shafakyildiz@gmail.com>
2024-09-15 12:49:57 +0300

    feat: implement addpolicyhandler to handle wait timeout in startup.cs

--- Play.Inventory/src/Play.Inventory.Service/Startup.cs
+++ Play.Inventory/src/Play.Inventory.Service/Startup.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -8,6 +9,7 @@ using Microsoft.OpenApi.Models;
 using Play.Common.MongoDB;
 using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
+using Polly;
 
 namespace Play.Inventory.Service
 {
@@ -28,7 +30,7 @@ namespace Play.Inventory.Service
             services.AddHttpClient<CatalogClient>(client =>
             {
                 client.BaseAddress = new Uri("https://localhost:5001");
-            });
+            }).AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {

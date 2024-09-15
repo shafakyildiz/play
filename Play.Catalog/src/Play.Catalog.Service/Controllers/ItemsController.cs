23440305a0a0ca734abb19493da73b0882ac2264
shafakyildiz <shafakyildiz@gmail.com>
2024-09-15 14:13:54 +0300

    fix: move files and fix project reference error

--- Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
+using Play.Catalog.Contracts;
 using Play.Catalog.Service.Dtos;
 using Play.Catalog.Service.Entities;
 using Play.Common;
@@ -16,33 +18,17 @@ namespace Play.Catalog.Service.Controllers
     public class ItemsController : ControllerBase
     {
         private readonly IRepository<Item> itemsRepository;
-        private static int requestCounter = 0;
-
-        public ItemsController(IRepository<Item> itemsRepository)
+        private readonly IPublishEndpoint publishEndpoint;
+        public ItemsController(IRepository<Item> itemsRepository, IPublishEndpoint publishEndpoint)
         {
             this.itemsRepository = itemsRepository;
+            this.publishEndpoint = publishEndpoint;
         }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<ItemDto>>> GetAsync()
         {
-            requestCounter++;
-            Console.WriteLine($"Request {requestCounter}: Starting...");
-
-            if (requestCounter <= 2)
-            {
-                Console.WriteLine($"Request {requestCounter}: Delaying...");
-                await Task.Delay(TimeSpan.FromSeconds(10));
-            }
-
-            if (requestCounter <= 4)
-            {
-                Console.WriteLine($"Request {requestCounter}: 500 (Internal Server Error)");
-                return StatusCode(500);
-            }
-
             var items = (await itemsRepository.GetAllAsync()).Select(item => item.AsDto());
-            Console.WriteLine($"Request {requestCounter}: 200 (OK)");
 
             return Ok(items);
         }
@@ -72,6 +58,9 @@ namespace Play.Catalog.Service.Controllers
             };
 
             await itemsRepository.CreateAsync(item);
+
+            await publishEndpoint.Publish(new CatalogItemCreated(item.Id, item.Name, item.Description));
+
             return CreatedAtAction(nameof(GetByIdAsync), new { id = item.Id }, item);
         }
 
